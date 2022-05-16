a45997b52399f6d77ff1eb0f095cc73b54e436d4
kpc1999kpc <kpc1999kpc@gmail.com>
2022-05-16 11:34:43 +0200

    test2

--- src/main/java/pl/bykowski/kolo5/Kolo5Application.java
+++ src/main/java/pl/bykowski/kolo5/Kolo5Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Kolo5Application {
-
+// comment
     public static void main(String[] args) {
         SpringApplication.run(Kolo5Application.class, args);
     }
